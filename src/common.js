1d22c85223cdf648acdee26725de4aecbd083b2a
flowerGu <1611121700@qq.com>
2017-06-29 19:47:09 +0800

    Create common.js

---
+++ src/common.js
@@ -0,0 +1,27 @@
+function LocalStore(key,value){
+    this.key = key;
+    this.value=value;
+}
+LocalStore.prototype={
+    // constructor : LocalStore,
+    set:function(key,value){
+        localStorage.setItem(key,typeof value == 'object'?JSON.stringify(value):value)
+    },
+    get:function(key){
+        return JSON.parse(localStorage.getItem(key));
+    },
+    remove:function(key){
+        return localStorage.removeItem(key);
+    },
+    clear:function(){
+        return localStorage.clear();
+    }
+}
+function getId(selector){
+    return document.getElementById(selector);
+}
+export default {
+    LocalStore,
+    getId
+}
+

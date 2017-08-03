9372f405ebf3fc667974cf36045a5c161813a2be
flowerGu <1611121700@qq.com>
2017-08-03 11:38:35 +0800

    Create ProducDetail.vue

---
+++ src/components/pages/ProducDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="productDetail">
+        {{$route.query.id}}222
+    </div>
+</template>
+<script>
+    export default{
+        mounted(){
+            console.log(this.$route.query.id)
+            this.$toast('233')
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>

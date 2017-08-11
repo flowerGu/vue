35ce0602b0c13d2b19b429bcb4bd834a801c08bd
flowerGu <1611121700@qq.com>
2017-08-11 16:54:51 +0800

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: ['./src/main.js','./src/js/flexble.js','./src/js/cookie.js']
+    app: ['./src/main.js','./src/js/flexble.js']
   },
   output: {
     path: config.build.assetsRoot,
